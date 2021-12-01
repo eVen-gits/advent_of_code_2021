0c197e053a25db1045aee021682bada649fe5259
even-gits <egej.vencelj@gmail.com>
2021-12-01 14:05:53 +0100

    *Update: Removed redundant list comprehensions thanks to /u/AP2008 suggestion

--- day_01/challenge0.py
+++ day_01/challenge0.py
@@ -1,17 +1,17 @@
 
 def pt_0(data):
-    descends = sum([
+    descends = sum(
         1 for
         i in range(1, len(data))
         if data[i] > data[i-1]
-    ])
+    )
     return descends
 
 def pt_1(data):
-    return sum([
+    return sum(
         1 if sum(data[i:i+3]) < sum(data[i+1:i+4]) else 0
         for i in range(0, len(data)-3)
-    ])
+    )
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
